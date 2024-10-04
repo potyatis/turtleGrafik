d87e572819f08ea4d2cebdb70ffddad3d7b5197d
Isak-OOO <isol07001@utb.vaxjo.se>
2024-10-04 09:01:04 +0200

    yeaah

---
+++ testEtt.py
@@ -0,0 +1,31 @@
+import turtle
+import random
+
+#setup
+screen = turtle.Screen()
+screen.setup(1000,800)
+screen.bgcolor("#000000")
+n = int(input("hur många taggar vill du ha"))
+t = turtle.Turtle()
+t.color("white","yellow")
+t.width(2)
+t.speed(0)
+
+def draw_star(t_obj,x, y, size):
+    angle = 120
+    t_obj.penup()
+    t_obj.goto(x,y)
+    t_obj.pendown()
+    t_obj.begin_fill()  
+    for _ in range(n):
+        t_obj.forward(size)
+        t_obj.right(angle)
+        t_obj.forward(size)
+        t_obj.right((360/n) - angle)
+    t.end_fill()
+
+for _ in range(50):
+    x, y = random.randint(-400,400), random.randint(-350,350)
+    draw_star(t,x,y,random.randint(10,20))
+
+screen.exitonclick()
